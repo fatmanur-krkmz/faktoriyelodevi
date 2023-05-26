70cf8d2dcdbfa056c65d7c499f0c25ead512f47d
User <User@USER-BILGISAYAR>
2023-05-26 15:09:52 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace faktoriyelodevi
+{
+    partial class İşlemler
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sayigirtextbox = new System.Windows.Forms.TextBox();
+            this.faktoriyelhasapla = new System.Windows.Forms.Button();
+            this.asalsayi = new System.Windows.Forms.Button();
+            this.sayilar = new System.Windows.Forms.Button();
+            this.faktsonuc = new System.Windows.Forms.Label();
+            this.asallabel = new System.Windows.Forms.Label();
+            this.sayilargoster = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.faktsonuctext = new System.Windows.Forms.TextBox();
+            this.asalmitext = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sayigirtextbox
+            // 
+            this.sayigirtextbox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.sayigirtextbox.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.sayigirtextbox.ForeColor = System.Drawing.Color.Indigo;
+            this.sayigirtextbox.Location = new System.Drawing.Point(356, 50);
+            this.sayigirtextbox.Name = "sayigirtextbox";
+            this.sayigirtextbox.Size = new System.Drawing.Size(100, 26);
+            this.sayigirtextbox.TabIndex = 1;
+            // 
+            // faktoriyelhasapla
+            // 
+            this.faktoriyelhasapla.BackColor = System.Drawing.Color.Plum;
+            this.faktoriyelhasapla.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.faktoriyelhasapla.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.faktoriyelhasapla.Location = new System.Drawing.Point(57, 176);
+            this.faktoriyelhasapla.Name = "faktoriyelhasapla";
+            this.faktoriyelhasapla.Size = new System.Drawing.Size(172, 32);
+            this.faktoriyelhasapla.TabIndex = 2;
+            this.faktoriyelhasapla.Text = "Faktöriyel Hesapla:";
+            this.faktoriyelhasapla.UseVisualStyleBackColor = false;
+            this.faktoriyelhasapla.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // asalsayi
+            // 
+            this.asalsayi.BackColor = System.Drawing.Color.Plum;
+            this.asalsayi.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.asalsayi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.asalsayi.Location = new System.Drawing.Point(443, 176);
+            this.asalsayi.Name = "asalsayi";
+            this.asalsayi.Size = new System.Drawing.Size(140, 32);
+            this.asalsayi.TabIndex = 4;
+            this.asalsayi.Text = "Asal Sayı mı?";
+            this.asalsayi.UseVisualStyleBackColor = false;
+            this.asalsayi.Click += new System.EventHandler(this.asalsayi_Click);
+            // 
+            // sayilar
+            // 
+            this.sayilar.BackColor = System.Drawing.Color.Plum;
+            this.sayilar.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.sayilar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.sayilar.Location = new System.Drawing.Point(311, 285);
+            this.sayilar.Name = "sayilar";
+            this.sayilar.Size = new System.Drawing.Size(185, 44);
+            this.sayilar.TabIndex = 6;
+            this.sayilar.Text = "Kendisine Kadar Olan Sayılar:";
+            this.sayilar.UseVisualStyleBackColor = false;
+            this.sayilar.Click += new System.EventHandler(this.sayilar_Click);
+            // 
+            // faktsonuc
+            // 
+            this.faktsonuc.AutoSize = true;
+            this.faktsonuc.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.faktsonuc.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.faktsonuc.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.faktsonuc.Location = new System.Drawing.Point(235, 179);
+            this.faktsonuc.Name = "faktsonuc";
+            this.faktsonuc.Size = new System.Drawing.Size(0, 23);
+            this.faktsonuc.TabIndex = 8;
+            // 
+            // asallabel
+            // 
+            this.asallabel.AutoSize = true;
+            this.asallabel.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.asallabel.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.asallabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.asallabel.Location = new System.Drawing.Point(637, 179);
+            this.asallabel.Name = "asallabel";
+            this.asallabel.Size = new System.Drawing.Size(0, 23);
+            this.asallabel.TabIndex = 9;
+            // 
+            // sayilargoster
+            // 
+            this.sayilargoster.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.sayilargoster.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.sayilargoster.ForeColor = System.Drawing.Color.Indigo;
+            this.sayilargoster.FormattingEnabled = true;
+            this.sayilargoster.ItemHeight = 18;
+            this.sayilargoster.Location = new System.Drawing.Point(345, 335);
+            this.sayilargoster.Name = "sayilargoster";
+            this.sayilargoster.Size = new System.Drawing.Size(120, 94);
+            this.sayilargoster.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Plum;
+            this.button1.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(637, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 48);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Yeni Sayı için Temizle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // faktsonuctext
+            // 
+            this.faktsonuctext.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.faktsonuctext.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.faktsonuctext.ForeColor = System.Drawing.Color.Indigo;
+            this.faktsonuctext.Location = new System.Drawing.Point(241, 179);
+            this.faktsonuctext.Name = "faktsonuctext";
+            this.faktsonuctext.Size = new System.Drawing.Size(100, 26);
+            this.faktsonuctext.TabIndex = 13;
+            // 
+            // asalmitext
+            // 
+            this.asalmitext.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.asalmitext.Font = new System.Drawing.Font("Verdana", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.asalmitext.ForeColor = System.Drawing.Color.Indigo;
+            this.asalmitext.Location = new System.Drawing.Point(610, 182);
+            this.asalmitext.Name = "asalmitext";
+            this.asalmitext.Size = new System.Drawing.Size(100, 26);
+            this.asalmitext.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Plum;
+            this.button2.Font = new System.Drawing.Font("Verdana", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button2.Location = new System.Drawing.Point(336, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 32);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Sayı Giriniz:";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // İşlemler
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.asalmitext);
+            this.Controls.Add(this.faktsonuctext);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sayilargoster);
+            this.Controls.Add(this.asallabel);
+            this.Controls.Add(this.faktsonuc);
+            this.Controls.Add(this.sayilar);
+            this.Controls.Add(this.asalsayi);
+            this.Controls.Add(this.faktoriyelhasapla);
+            this.Controls.Add(this.sayigirtextbox);
+            this.Name = "İşlemler";
+            this.Text = "İşlemler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox sayigirtextbox;
+        private System.Windows.Forms.Button faktoriyelhasapla;
+        private System.Windows.Forms.Button asalsayi;
+        private System.Windows.Forms.Button sayilar;
+        private System.Windows.Forms.Label faktsonuc;
+        private System.Windows.Forms.Label asallabel;
+        private System.Windows.Forms.ListBox sayilargoster;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox faktsonuctext;
+        private System.Windows.Forms.TextBox asalmitext;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace faktoriyelodevi
+{
+    public partial class İşlemler : Form
+    {
+        int sayi;
+      
+        public İşlemler()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            sayi = Convert.ToInt32(sayigirtextbox.Text);
+            int fakt = 1;
+            for(int i=1; sayi >= i; i++)
+            {
+                fakt *= i;
+            }
+            faktsonuctext.Text = fakt.ToString();
+        }
+
+        private void asalsayi_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            for (int i = 1; i <= sayi; i++)
+            {
+                
+                if (sayi % i == 0)
+                {
+                    sayac++;
+                }
+            }
+            if (sayac == 2)
+            {
+                asalmitext.Text = "Evet.";
+            }
+            else
+            {
+                asalmitext.Text = "Hayır.";
+
+            }
+        }
+
+        private void sayilar_Click(object sender, EventArgs e)
+        {
+            sayi = Convert.ToInt32(sayigirtextbox.Text);
+            int[] sayilar = new int[100];
+            for (int i = 0; sayi >= i; i++)
+            {
+                
+                sayilar[i] = i+1;
+                
+            }
+            for (int a = 0; a < sayi; a++)
+            {
+                sayilargoster.Items.Add(sayilar[a]);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            sayigirtextbox.Clear();
+            faktsonuctext.Clear();
+            asalmitext.Clear();
+            sayilargoster.Items.Clear();
+            
+        }
+    }
+}
